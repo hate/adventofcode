9ab1c58994578ff340c8ec207e326dcda4ceb804
lelo <15314665+hate@users.noreply.github.com>
2022-12-01 15:14:56 -0500

    initial

---
+++ 2022/day01-CalorieCounting/src/main.rs
@@ -0,0 +1,12 @@
+// reindeer need magical energy to delive presents
+// reindeer eat stars
+// need to retrieve 50 stars by dec 25
+// 2 puzzles are available each day
+
+// elves take inventory of the Calories each one is carrying
+// elves want to figure out the one with the highest count
+// inventory found in input.txt
+
+fn main() {
+    
+}
